d09344ad18d89cd78771ac554351e78474ff6c9a
depp57 <deppdu5758@gmail.com>
2021-04-19 19:45:55 +0200

    better performance for 404 redirection

--- web-client/src/app/shared/services/page-not-found.guard.ts
+++ web-client/src/app/shared/services/page-not-found.guard.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { RedirectReasons } from '@shared/constants';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class PageNotFoundGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private auth: AuthService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    this.router.navigate([''], {state: {redirect: RedirectReasons.PAGE_NOT_FOUND}});
+    if (this.auth.isAuthenticated()) {
+      this.router.navigate(['fichiers'], {state: {redirect: RedirectReasons.PAGE_NOT_FOUND}});
+    }
+    else {
+      this.router.navigate([''], {state: {redirect: RedirectReasons.PAGE_NOT_FOUND}});
+    }
     return false;
   }
 }
