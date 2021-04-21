648abe6087a7f7af325c297127197cada533a965
Sven Thommet <sven.thommet@gmail.com>
2021-04-21 22:28:55 +0200

    renaming creds struct name

--- server/client-EP/http-handles.go
+++ server/client-EP/http-handles.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/sventhommet/cloud-storage/server/utils"
 )
 
-type AuthCouple struct {
+type Creds struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -21,27 +19,21 @@ func handleAuth(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	// request.ParseForm()
-	// var username = request.PostFormValue("username")
-	// var password = request.PostFormValue("password")
-
-	var authC AuthCouple
+	var creds Creds
 	result, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(result))
-	err2 := json.Unmarshal(result, &authC)
+	err2 := json.Unmarshal(result, &creds)
 	if err2 != nil {
 		panic(err2)
 	}
 
 	fmt.Println("Trying to authenticate...")
-	fmt.Println("user : " + authC.Username)
-	fmt.Println("password : " + authC.Password)
-	fmt.Println("password_sha256 : " + utils.Sha256(authC.Password) + "\n")
+	fmt.Println("user : " + creds.Username)
+	fmt.Println("password : " + creds.Password)
 
-	token, err := auth.Connect(authC.Username, authC.Password)
+	token, err := auth.Connect(creds.Username, creds.Password)
 	if err != nil {
 		response.WriteHeader(401)
 		response.Write([]byte("{\"error\": \"" + err.Error() + "\"}"))
@@ -71,7 +63,7 @@ func handleFilesList(response http.ResponseWriter, request *http.Request) {
 
 func handleFilesDL(response http.ResponseWriter, request *http.Request) {
 	if request.Method != "GET" {
-		response.WriteHeader(501)
+		response.WriteHeader(405)
 		response.Write([]byte("{error: must be GET method}"))
 		return
 	}
@@ -82,8 +74,3 @@ func handleFilesDL(response http.ResponseWriter, request *http.Request) {
 
 	return
 }
-
-// func addAngularHeaders(response *http.ResponseWriter) {
-// 	response.Add("Content-Type", "application/json")
-// 	response.Add("Access-Control-Allow-Origin", "*")
-// }
