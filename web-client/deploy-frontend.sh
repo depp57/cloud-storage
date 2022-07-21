941de01e23949607008e989af6c069f4ba80b889
Ubuntu <ubuntu@vps-a205acd4.vps.ovh.net>
2022-07-21 13:45:19 +0000

    chore(webclient container): update webclient docker container conf

--- web-client/deploy-frontend.sh
+++ web-client/deploy-frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 80:80 -p 443:443 --restart=always web-client
+docker run -d -p 80:80 -p 443:443 --restart=always registry.gitlab.com/sthommet/cloud-storage/web-client
 echo "done! The wep app is running."
